178fb9c6986664164bb52c0458cfd8d2bcaf6606
Christopher <morochito344@gmail.com>
2025-07-06 22:43:02 -0500

    si

---
+++ controllers/LoginController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController {
+
+    public static function login(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+
+        // Render a la vista
+        $router->render('auth/login', [
+            'titulo' => 'Iniciar Sesión'
+        ]);
+    }
+
+    public static function logout() {
+        echo 'Desde logout';
+    }
+
+    public static function crear(Router $router) {
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+
+        // Render a la vista
+        $router->render('auth/crear', [
+            'titulo' => 'Crear tu cuenta en UpTask'
+        ]);
+    }
+
+    public static function olvide() {
+        echo 'Desde Olvide';
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+
+    
+    public static function reestablecer() {
+        echo 'Desde Reestablecer';
+
+        if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+        }
+    }
+
+    public static function mensaje() {
+        echo 'Desde Mensaje';
+    }
+
+
+    public static function confirmar() {
+        echo 'Desde Confirmar';
+    }
+}
---
+++ views/auth/login.php
@@ -0,0 +1,37 @@
+<div class="contenedor">
+    <h1>UpTask</h1>
+    <p class="tagline">Crea y administra tus proyectos</p>
+
+    <div class="contenedor-sm">
+        <p class="descripcion-pagina">Iniciar Sesión</p>
+
+        <form action="/" class="formulario" method="POST">
+            <div class="campo">
+                <label for="email">Email</label>
+                <input
+                    type="email"
+                    id="email"
+                    placeholder="Tu e-mail"
+                    name="email"
+                />
+            </div>
+
+
+            <div class="campo">
+                <label for="email">Contraseña</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Tu contraseña"
+                    name="password"
+                />
+            </div>
+        <input type="submit" class="boton" value="Iniciar Sesión">
+        </form>
+
+        <div class="acciones">
+            <a href="/crear">Aún no tienes una cuenta? Crear una!</a>
+            <a href="/olvide">Olvidé Contraseña</a>
+        </div>
+  </div>    <!-- contenedor sm -->
+</div>
